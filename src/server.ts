391a9c717e80aef6323ce158c7f40395ab719a76
JuniorQb <eucomcerteza@gmail.com>
2024-03-15 19:23:20 -0300

    configure typeScript

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify';
+
+
+const app = fastify();
+
+app.get('/', async(req, res)=> {
+  
+})
+
+app.listen({
+  port: 3333
+}).then(()=> {
+  console.log('server online')
+})
