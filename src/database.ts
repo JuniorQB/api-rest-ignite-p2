d1fcd7f8056f43834251b95f4cabe1045b1e5371
JuniorQb <eucomcerteza@gmail.com>
2024-03-15 21:49:36 -0300

    installed knex

---
+++ src/database.ts
@@ -0,0 +1,9 @@
+import { knex as setupKnex } from 'knex'
+
+export const knex = setupKnex({
+  client: 'sqlite3', // or 'better-sqlite3'
+  connection: {
+    filename: './mydb.sqlite',
+  },
+  useNullAsDefault: true,
+})
