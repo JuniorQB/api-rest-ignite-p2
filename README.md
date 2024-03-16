f1ea5da7b033e20a755f338b85776a566e24965b
JuniorQb <eucomcerteza@gmail.com>
2024-03-15 23:32:22 -0300

    created database typing validation, created route files and configured fastify plugins

---
+++ README.md
@@ -0,0 +1,13 @@
+# RF
+
+- [ ] o usuario deve poder criar uma nova transação 
+- [ ] o usuario deve poder obter um resumo da sua conta
+- [ ] o usuário deve poder listar todas as transaçõies que já ocorreram 
+- [ ] o usuario deve poder visualizar uma transação unica
+
+# RN 
+- [ ] transação pode ser do tipo crédito que somará ao valor total, ou débito subttairá
+- [ ] Deve ser possível identificarmos o usuário entre as requisições 
+- [ ] o usuário só pode visualizar transações o qual ele criou
+
+# RNF
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async (req, res) => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
