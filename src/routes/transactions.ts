0e3f527ddf3b9491a9134a2bbdb10bab0636e621
JuniorQb <eucomcerteza@gmail.com>
2024-03-15 23:39:04 -0300

    created route get transactions by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,25 @@ import { knex } from '../database'
 import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+    return { transactions }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const params = getTransactionParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions')
+      .where('id', params.id)
+      .first()
+
+    return { transaction }
+  })
+
   app.post('/', async (req, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
